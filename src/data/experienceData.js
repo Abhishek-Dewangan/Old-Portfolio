9a93886a148e26dcecf36b53bdb2187262dc8ed1
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-07 14:37:01 +0530

    Education section update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,16 +1,16 @@
 export const experienceData = [
-//   {
-//     id: 1,
-//     company: 'Masai School',
-//     jobtitle: 'Full Stack Web Developer Training',
-//     startYear: 'Dec 2021 - Present',
-//   },
-//   {
-//     id: 2,
-//     company: 'Masai School',
-//     jobtitle: 'Student Guide',
-//     startYear: 'Apr 2022 - May 2022',
-//   },
+  {
+    id: 1,
+    company: 'Masai School',
+    jobtitle: 'Full Stack Web Developer Training',
+    startYear: 'Dec 2021 - Present',
+  },
+  {
+    id: 2,
+    company: 'Masai School',
+    jobtitle: 'Student Guide',
+    startYear: 'Apr 2022 - May 2022',
+  },
   {
     id: 3,
     company: 'Chhattisgarh State Power Transmission Company Limited',
